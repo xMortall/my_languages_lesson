244386df3c7d6d758d7f717cbb610bf84906da5d
xMortal <emanuelborges13998@gmail.com>
2023-11-03 14:26:27 +0000

    update

---
+++ Funcoes.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int myArray[] = { 1, 2, 3, 4, 5 };
+
+string letras[2][4];
+
+string letras2[2][4] = {
+    {"A","B","C","D"},
+    {"E","F","G","H"}
+};
+
+int main(){
+    for (int i = 0; i < 2; i++){
+        for (int j = 0; j < 2; j++){
+        cout << letras2[i][j] << endl;
+        }
+    }
+
+    return 0;
+};
