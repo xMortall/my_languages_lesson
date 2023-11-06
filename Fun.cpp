c4db1485b841a419635dc26bf3d737ee0234864f
xMortal <emanuelborges13998@gmail.com>
2023-11-06 10:41:28 +0000

    ola

--- Fun.cpp
+++ Fun.cpp
@@ -5,41 +5,56 @@ using namespace std;
 int main() {
     char operacao;
     double numero1, numero2, resultado;
+    bool continuar = true;
 
-    cout << "Calculadora Simples em C++" << endl;
-    cout << "Digite a operação (+, -, *, /): ";
-    cin >> operacao;
+    while (continuar) {
+        cout << "Digite 's' para calcular, 'q' para sair: ";
+        cin >> operacao;
 
-    cout << "Digite o primeiro número: ";
-    cin >> numero1;
-
-    cout << "Digite o segundo número: ";
-    cin >> numero2;
-
-    switch (operacao) {
-        case '+':
-            resultado = numero1 + numero2;
-            cout << "Resultado: " << resultado << endl;
+        if (operacao == 'q' || operacao == 'Q') {
+            continuar = false;
             break;
-        case '-':
-            resultado = numero1 - numero2;
-            cout << "Resultado: " << resultado << endl;
-            break;
-        case '*':
-            resultado = numero1 * numero2;
-            cout << "Resultado: " << resultado << endl;
-            break;
-        case '/':
-            if (numero2 != 0) {
-                resultado = numero1 / numero2;
+        }
+
+        if (operacao != 's' && operacao != 'S') {
+            cout << "Opção inválida. Digite 's' para calcular ou 'q' para sair." << endl;
+            continue;
+        }
+
+        cout << "Digite a operação (+, -, *, /): ";
+        cin >> operacao;
+
+        cout << "Digite o primeiro número: ";
+        cin >> numero1;
+
+        cout << "Digite o segundo número: ";
+        cin >> numero2;
+
+        switch (operacao) {
+            case '+':
+                resultado = numero1 + numero2;
                 cout << "Resultado: " << resultado << endl;
-            } else {
-                cout << "Erro: Divisão por zero!" << endl;
-            }
-            break;
-        default:
-            cout << "Operação inválida. Por favor, digite +, -, *, ou /." << endl;
-    }   
+                break;
+            case '-':
+                resultado = numero1 - numero2;
+                cout << "Resultado: " << resultado << endl;
+                break;
+            case '*':
+                resultado = numero1 * numero2;
+                cout << "Resultado: " << resultado << endl;
+                break;
+            case '/':
+                if (numero2 != 0) {
+                    resultado = numero1 / numero2;
+                    cout << "Resultado: " << resultado << endl;
+                } else {
+                    cout << "Erro: Divisão por zero!" << endl;
+                }
+                break;
+            default:
+                cout << "Operação inválida. Por favor, digite +, -, *, ou /." << endl;
+        }
+    }
 
     return 0;
 }
