9b1a1ecfdc022bd8309c431c261b2d7621210af9
xMortal <emanuelborges13998@gmail.com>
2023-11-29 13:33:22 +0000

    ola

--- Python/password.py
+++ Python/password.py
@@ -1,23 +1,26 @@
 import subprocess
 
-command = subprocess.check_output(['/usr/sbin/iwconfig']).decode('utf-8').split('\n')
-wireless_interfaces = [line.split(" ")[0] for line in command if "IEEE 802.11" in line]
+def get_networks():
+    information_command = subprocess.check_output("netsh wlan show profiles", encoding="cp858", shell=True)
+    networks = []
+    for line in information_command.split('\n'):
+        if "Todos os Perfis de Usuários" in line:
+            position = line.find(":")
+            network = line[position + 2:]
+            password = information_network(network)
+            networks.append(network + " - " + password)
+    return networks
 
-# Use a primeira interface sem fio encontrada
-if wireless_interfaces:
-    interface_name = wireless_interfaces[0]
-else:
-    print("Nenhuma interface sem fio encontrada.")
-    exit()
+def information_network(wifi):
+    information_command = subprocess.check_output(["netsh", "wlan", "show", "profile", wifi, "key", "=", "clear"],
+                                                  encoding="cp858", shell=True)
+    network = ''
+    for line in information_command.split('\n'):
+        if "Conteúdo da Chave" in line:
+            position = line.find(":")
+            network = line[position + 2:]
+            break
+    return network
 
-command = subprocess.check_output(['iwlist', interface_name, 'scan']).decode('utf-8').split('\n')
-profiles = [i.split(":")[1][1:-1] for i in command if "ESSID" in i]
-
-for i in profiles:
-    try:
-        results = subprocess.check_output(['iwconfig', interface_name, 'essid', i]).decode('utf-8').split('\n')
-        results = [b.split(" ")[22][6:] for b in results if "IE: IEEE 802.11i/WPA2" in b]
-        print("{:<30}|  {:<}".format(i, results[0]))
-    except IndexError:
-        print("{:<30}|  {:<}".format(i, ""))
-input("")
+if __name__ == "__main__":
+    print(get_networks())
