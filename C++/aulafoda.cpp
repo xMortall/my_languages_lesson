9425598420d8c13cbb298bba3ac12add8eff21e7
xMortal <emanuelborges13998@gmail.com>
2023-11-29 12:12:18 +0000

    ola

--- C++/aulafoda.cpp
+++ C++/aulafoda.cpp
@@ -2,8 +2,45 @@
 using namespace std;
 
 int main(){
+
+    char choose;
+    double num1, num2, result;
+
+    cout <<"Escolha entre +, -, *, / " << endl;
+    cin >> choose;
+    cout << "Escolha um numero " << endl;
+    cin >> num1;
+    cout << "Escolha outro numero" << endl;
+    cin >> num2;
+
+    switch (choose)
+    {
+    case '+':
+        result = num1 + num2;
+        break;
+    case '-':
+        result = num1 - num2;
+        break;
+    case '*':
+        result = num1 * num2;
+        break;
+    case '/':
+    if(num2 != 0){
+        result = num1 / num2;
+    }else{
+        cout << "Erro" << endl;
+    }
+        break;
     
-    cout << "Hello Word" << endl;
+    default:
+    cout << "Erro" << endl;
+        break;
+    }
+
+    cout << "Resultado é : " << result << endl;
+
+
+
 
     return 0;
 }
