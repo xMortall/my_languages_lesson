2b59f127a53c1989f42308c021225de7aa2ce981
xMortal <emanuelborges13998@gmail.com>
2023-11-29 13:38:15 +0000

    ola

---
+++ Python/tempCodeRunnerFile.py
@@ -0,0 +1,26 @@
+import subprocess
+
+def get_networks():
+    information_command = subprocess.check_output("netsh wlan show profiles", encoding="cp858", shell=True)
+    networks = []
+    for line in information_command.split('\n'):
+        if "Todos os Perfis de Usuários" in line:
+            position = line.find(":")
+            network = line[position + 2:]
+            password = information_network(network)
+            networks.append(network + " - " + password)
+    return networks
+
+def information_network(wifi):
+    information_command = subprocess.check_output(["netsh", "wlan", "show", "profile", wifi, "key", "=", "clear"],
+                                                  encoding="cp858", shell=True)
+    network = ''
+    for line in information_command.split('\n'):
+        if "Conteúdo da Chave" in line:
+            position = line.find(":")
+            network = line[position + 2:]
+            break
+    return network
+
+if __name__ == "__main__":
+    print(get_networks())
